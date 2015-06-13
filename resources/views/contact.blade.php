aa3f45a8055ab6f43a20c8f55876d883dd183865
franzbel <franzbeltran79@hotmail.com>
2015-06-12 20:48:14 -0400

    listado de posts

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,24 +1,71 @@
 @extends('app')
 
-@section('title', 'Hola mundo')
+
+{{--@section('content')--}}
+{{--<h1>TODOS LOS POSTS</h1>--}}
+{{--{!! Form::open(array('route' => 'posts.store', 'method'=>'post')) !!}--}}
+{{--@include('movies.partials.form')--}}
+
+{{--{!! Form::hidden('user_id',  Auth::id()) !!}--}}
+{{--<div class="form-group">--}}
+{{--{!! Form::textarea('texto',null, ['class'=>'form-control']) !!}--}}
+{{--</div>--}}
+
+{{--<button type="submit" class="btn btn-info">Postear</button>--}}
+{{--{!! Form::close() !!}--}}
+
+
+{{--<div class="fondoguest">--}}
+{{--<p>{{$mensaje}}</p>--}}
+{{--</div>--}}
+{{--<p>comoe es </p>--}}
+{{--@stop--}}
 
 @section('content')
-    <h1>TODOS LOS POSTS</h1>
-    {!! Form::open(array('route' => 'posts.store', 'method'=>'post')) !!}
-    {{--@include('movies.partials.form')--}}
 
-        {!! Form::hidden('user_id',  Auth::id()) !!}
-        <div class="form-group">
-            {!! Form::textarea('texto',null, ['class'=>'form-control']) !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <h1>TODOS LOS POSTS</h1>
+                    <button class="btn btn-info btn-sm">Seguir</button>
+                    <div class="panel-body">
+
+
+                        {!! Form::open(array('route' => 'posts.store', 'method'=>'post')) !!}
+                        {!! Form::hidden('user_id',  Auth::id()) !!}
+                        <div class="form-group">
+                            {!! Form::textarea('texto',null, ['class'=>'form-control']) !!}
+                        </div>
+                        <button type="submit" class="btn btn-info">Postear</button>
+                        {!! Form::close() !!}
+
+                        <table class="table-striped table-hover">
+                            <thead>
+                            <tr >
+                                <th>Posts</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach(App\User::find(Auth::id())->first()->posts as $post)
+                                <tr>
+                                    <td>{{$post->texto}}</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td><button class="btn btn-info btn-sm">Repostear</button></td>
+                                    <td><button class="btn btn-info btn-sm">Me gusta</button></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
         </div>
+    </div>
 
-        <button type="submit" class="btn btn-info">Postear</button>
-    {!! Form::close() !!}
+@endsection
 
-
-
-	<div class="fondoguest">
-		<p>{{$mensaje}}</p>
-	</div>
-    <p>comoe es </p>
-@stop
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,28 +1,56 @@
 @extends('app')
 
-@section('title', 'Hola mundo')
-
+<div class="fondoguest">
+</div>
+@foreach ($users as $user)
+    {{--{{$user->username}}--}}
+@endforeach
+{{--@stop--}}
 @section('content')
-	<h1>perfil</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <h1>SOLO MIS POSTS</h1>
+                    <button class="btn btn-info btn-sm">Seguir</button>
+                    <div class="panel-heading">Bienvenido {{$user->name}}</div>
+                    <div class="panel-body">
 
 
-    {!! Form::open(array('route' => 'posts.store', 'method'=>'post')) !!}
-    {{--@include('movies.partials.form')--}}
+                        {!! Form::open(array('route' => 'posts.store', 'method'=>'post')) !!}
+                        {!! Form::hidden('user_id',  Auth::id()) !!}
+                        <div class="form-group">
+                            {!! Form::textarea('texto',null, ['class'=>'form-control']) !!}
+                        </div>
+                        <button type="submit" class="btn btn-info">Postear</button>
+                        {!! Form::close() !!}
 
-    {!! Form::hidden('user_id',  Auth::id()) !!}
-    <div class="form-group">
-        {!! Form::textarea('texto',null, ['class'=>'form-control']) !!}
+                        <table class="table-striped table-hover">
+                            <thead>
+                            <tr >
+                                <th>Posts</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach(App\User::find(Auth::id())->first()->posts as $post)
+                                <tr>
+                                    <td>{{$post->texto}}</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td><button class="btn btn-info btn-sm">Repostear</button></td>
+                                    <td><button class="btn btn-info btn-sm">Me gusta</button></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 
-    <button type="submit" class="btn btn-info">Postear</button>
-    {!! Form::close() !!}
-
-
-	<div class="fondoguest">
-		</div>
-		@foreach ($users as $user)
-		{{$user->username}}
-		
-		
-	@endforeach
-@stop
+@endsection
